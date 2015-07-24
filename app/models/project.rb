d1867a241485bffb26b07ad1ae74ee90da6e0caa
Emma Leary <eleary01@qub.ac.uk>
2015-07-24 23:56:45 +0100

    validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,11 @@
 class Project < ActiveRecord::Base
+  validates :name, presence: true,
+            length: { minimum: 4 }
+
+  validates :languages, presence: true,
+            length: { minimum: 4 }
+
+  validates :description, presence: true,
+            length: { minimum: 4 }
+
 end
